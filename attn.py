a5f53769f8550e03e2dcc7489d5d779fbfac1c6c
Shuyang Shi <shuyang790@gmail.com>
2018-03-19 17:54:23 -0700

    Update attention visualization: self-attention

--- attn.py
+++ attn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 
 bidaf_attn_c2q = np.load("./log/bidaf-attn-c2q.log")
 bidaf_attn_q2c = np.load("./log/bidaf-attn-q2c.log")
@@ -14,21 +15,35 @@ with open("./log/samples.log", "r") as f:
 
 # attn: size [context_len, *]
 def plotAttn(attn, sample):
+    context = "norman architecture typically stands out as a new stage in the architectural history of the regions they subdued . they spread a unique romanesque idiom to england and italy , and the encastellation of these regions with keeps in their north french style fundamentally altered the military landscape . their style was characterised by rounded arches , particularly over windows and doorways , and massive proportions ."
     attn = np.squeeze(attn)
-    # print "attn.shape={}".format(attn.shape)
-    if len(attn.shape) > 1:
-        norms = np.linalg.norm(attn, axis=1)  # size (context_len)
-    else:
-        norms = attn
+    print "attn.shape={}".format(attn.shape)
     L = len(sample[0].split(' ')) - 1
     # print "L={}".format(L)
-    plt.clf()
-    xs = range(1, L+1)
-    ys = norms[:L]
-    plt.plot(xs, ys, '.-')
-    plt.title(sample[1] + sample[2])
-    plt.xlabel("position in context")
-    plt.ylabel("attention norm")
+    data = attn[:L, :L]
+    # fig, axes = plt.subplot(1, 1)
+    # xs = range(1, L+1)
+    # ys = norms[:L]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cmap = colors.ListedColormap([
+        (1. - x * 0.01, 1. - x * 0.005, 1. - x * 0.01)
+        for x in range(100)
+    ])
+    ax.set_xticklabels(context.split(" "), rotation=90)
+    ax.set_yticklabels(context.split(" "))
+    ax.tick_params(axis='both', width=1, top='off', labelsize=6)
+    plt.xticks(range(0, L))
+    plt.yticks(range(0, L))
+    # plt.xlabel(context.split(" "))
+    # plt.ylabel(context.split(" "))
+    # plt.xticks(rotation=90)
+    # plt.pcolor(attn[:L, :L])
+    ax.imshow(attn[:L, :L], cmap=cmap, interpolation='nearest')
+    # plt.plot(xs, ys, '.-')
+    # plt.title(sample[1] + sample[2])
+    # plt.xlabel("position in context")
+    # plt.ylabel("attention norm")
     plt.show()
 
 
